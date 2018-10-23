7026e9840e5a4f05280346db979ffd5193b95272
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-10-23 12:38:08 +0400

    able to read first device in name_prefix only

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ let mainWindow;
 
 function createWindow() {
 	// Create the browser window.
-	mainWindow = new BrowserWindow({width: 800, height: 600});
+	mainWindow = new BrowserWindow({width: 1100, height: 800});
+	let contents = mainWindow.webContents;
+	contents.executeJavaScript('console.log(this)')
+		.then(console.log)
+		.catch(console.error);
 
 	// and load the index.html of the app.
 	// mainWindow.loadURL('http://localhost:3000');
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,36 @@
 import React, {Component} from 'react';
 import { Button } from 'reactstrap';
+//import Bluetooth from 'node-web-bluetooth';
 
+
+// async function connect() {
+// 	const device = await Bluetooth.requestDevice(options);
+// 	const server = await device.connect();
+// 	const service = await server.getPrimaryService('heart_rate');
+// 	const char = await service.getCharacteristic('heart_rate_measurement');
+// 	await char.startNotifications();
+// 	char.on('characteristicvaluechanged', (data) => {
+// 		// parse heart-rate data here
+// 	});
+// 	await char.stopNotifications();
+// 	await server.disconnect();
+// }
 
 class Home extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			peripherals: 'Hello'
+			peripherals: 'Hello',
+			options : {
+				filters: [
+					{namePrefix:'Nokia'},
+					{services: ['heart_rate']},
+					{services: [0x1802, 0x1803]},
+					{services: ['c48e6067-5295-48d3-8d5c-0395f61792b1']},
+					{name: 'ExampleName'}
+				],
+				optionalServices: ['battery_service']
+			}
 		};
 
 		this.handleClick = this.handleClick.bind(this);
@@ -15,7 +39,16 @@ class Home extends Component {
 
 	}
 	handleClick() {
-		console.log("clicked");
+		//connect();
+		navigator.bluetooth.requestDevice(this.state.options).then(function(device) {
+			console.log('Name: ' + device.name);
+			this.setState(Object.assign({}, this.state, {peripherals: 'Yalla'}));
+			// Do something with the device.
+		})
+			.catch(function(error) {
+				console.log("Something went wrong. " + error);
+			});
+
 	}
 
 	render() {
