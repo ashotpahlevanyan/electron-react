e78c1bd9fdccd02f278d19599867cc63a0535d83
Ashot Pahlevanyan <ashotpahlevanyan@users.noreply.github.com>
2019-02-22 02:07:35 +0400

    Enable web bluetooth

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const { ipcMain } = require('electron');
 
 const path = require('path');
 const url = require('url');
+app.commandLine.appendSwitch(‘enable-experimental-web-platform-features’, true);
+app.commandLine.appendSwitch('enable-web-bluetooth', true);
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
